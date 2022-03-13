007caff3dca5c6684a9599b75d01c203e33c7eb1
olle-henriksson-wiren-nti-johanneberg <olle.henrikssonwiren@elev.ga.ntig.se>
2022-03-13 17:49:25 +0100

    Fixade knappar för både mobil och dator

--- js/script.js
+++ js/script.js
@@ -16,6 +16,18 @@ function toggleMeny() {
 
 burger.addEventListener('click', toggleMeny)
 
+function toggleCart() {
+
+    cart.classList.toggle('cartUnactive')
+}
+
+burger.addEventListener('click', toggleCart)
+
+
+
+
+
+
 // Färgknappar för produkter
 const blueimage = document.querySelector('.blueProductImg')
 const bluebutton = document.querySelector('.blueB')
@@ -28,6 +40,26 @@ const productimage = document.querySelector('.productImg')
 var desktopsize = window.matchMedia("(min-width: 480px)")
 
 function toggleBlack(desktopsize) {
+    if (window.matchMedia("(min-width: 480px").matches) {
+
+    document.getElementById("productImg").src = "../img/rick-muigo-SX9IouL_qOg-unsplash_black.jpg"
+
+    document.getElementById("blackB").style.boxShadow = "0 0 0 5px black"    
+    document.getElementById("blackB").style.border = "1.5em outset #585859"
+    document.getElementById("whiteB").style.boxShadow = "none"
+    document.getElementById("redB").style.boxShadow = "none"
+    document.getElementById("blueB").style.boxShadow = "none"
+
+    document.getElementById("redB").style.border = "1em outset red"
+    document.getElementById("whiteB").style.border = "1em outset #F2F2F2"
+    document.getElementById("blueB").style.border = "1em outset blue"
+
+    document.getElementById("blackB").style.transform = "rotate(360deg)"
+    document.getElementById("blueB").style.transform = "rotate(0deg)"
+    document.getElementById("redB").style.transform = "rotate(0deg)"
+    document.getElementById("whiteB").style.transform = "rotate(0deg)"
+
+    } else {
     document.getElementById("productImg").src = "../img/rick-muigo-SX9IouL_qOg-unsplash_black.jpg"
 
     document.getElementById("blackB").style.boxShadow = "0 0 0 3px white"    
@@ -44,12 +76,31 @@ function toggleBlack(desktopsize) {
     document.getElementById("blueB").style.transform = "rotate(0deg)"
     document.getElementById("redB").style.transform = "rotate(0deg)"
     document.getElementById("whiteB").style.transform = "rotate(0deg)"
+    }
 
     }
 
-
-    // funkar inte
 function toggleWhite(desktopsize) {
+    if (window.matchMedia("(min-width: 480px)").matches) {
+    document.getElementById("productImg").src ="../img/rick-muigo-SX9IouL_qOg-unsplash_white.jpg"
+
+    document.getElementById("whiteB").style.boxShadow = "0 0 0 5px black"    
+    document.getElementById("whiteB").style.border = "1.5em outset white"
+    document.getElementById("blackB").style.boxShadow = "none"
+    document.getElementById("redB").style.boxShadow = "none"
+    document.getElementById("blueB").style.boxShadow = "none"
+
+    document.getElementById("blueB").style.border = "1em outset blue"
+    document.getElementById("blackB").style.border = "1em outset #585859"
+    document.getElementById("redB").style.border = "1em outset red"
+
+    document.getElementById("whiteB").style.transform = "rotate(360deg)"
+    document.getElementById("blueB").style.transform = "rotate(0deg)"
+    document.getElementById("redB").style.transform = "rotate(0deg)"
+    document.getElementById("blackB").style.transform = "rotate(0deg)"
+
+
+    } else {
     document.getElementById("productImg").src ="../img/rick-muigo-SX9IouL_qOg-unsplash_white.jpg"
 
     document.getElementById("whiteB").style.boxShadow = "0 0 0 3px black"    
@@ -66,13 +117,31 @@ function toggleWhite(desktopsize) {
     document.getElementById("blueB").style.transform = "rotate(0deg)"
     document.getElementById("redB").style.transform = "rotate(0deg)"
     document.getElementById("blackB").style.transform = "rotate(0deg)"
-
-
+    }
 
     // funkar
 
 }
-function toggleBlue(desktopsize) {
+function toggleBlue(desktopsize) { //för 480px och över, alltså desktop
+    if (window.matchMedia("(min-width: 480px)").matches) {
+    document.getElementById("productImg").src ="../img/rick-muigo-SX9IouL_qOg-unsplash_blue.jpg"
+
+    document.getElementById("blueB").style.boxShadow = "0 0 0 5px black"
+    document.getElementById("blueB").style.border = "1.5em outset blue"
+    document.getElementById("whiteB").style.boxShadow = "none"
+    document.getElementById("redB").style.boxShadow = "none"
+    document.getElementById("blackB").style.boxShadow = "none"
+
+    document.getElementById("redB").style.border = "1em outset red"
+    document.getElementById("blackB").style.border = "1em outset #585859"
+    document.getElementById("whiteB").style.border = "1em outset #F2F2F2"
+
+    document.getElementById("blueB").style.transform = "rotate(360deg)"
+    document.getElementById("redB").style.transform = "rotate(0deg)"
+    document.getElementById("whiteB").style.transform = "rotate(0deg)"
+    document.getElementById("blackB").style.transform = "rotate(0deg)"
+
+    } else { //För mobil
     document.getElementById("productImg").src ="../img/rick-muigo-SX9IouL_qOg-unsplash_blue.jpg"
 
     document.getElementById("blueB").style.boxShadow = "0 0 0 3px white"
@@ -89,12 +158,30 @@ function toggleBlue(desktopsize) {
     document.getElementById("redB").style.transform = "rotate(0deg)"
     document.getElementById("whiteB").style.transform = "rotate(0deg)"
     document.getElementById("blackB").style.transform = "rotate(0deg)"
-
+    }
 
     // funkar
 
-}
+} //Röd bullshit
 function toggleRed(desktopsize) {
+    if (window.matchMedia("(min-width: 480px)").matches) {
+    document.getElementById("productImg").src = "../img/rick-muigo-SX9IouL_qOg-unsplash_red.jpg"
+
+    document.getElementById("redB").style.boxShadow = "0 0 0 5px black"
+    document.getElementById("redB").style.border = "1.5em outset red"
+    document.getElementById("whiteB").style.boxShadow = "none"
+    document.getElementById("blackB").style.boxShadow = "none"
+    document.getElementById("blueB").style.boxShadow = "none"
+    
+    document.getElementById("blueB").style.border = "1em outset blue"
+    document.getElementById("blackB").style.border = "1em outset #585859"
+    document.getElementById("whiteB").style.border = "1em outset #F2F2F2"
+    
+    document.getElementById("redB").style.transform = "rotate(360deg)"
+    document.getElementById("blueB").style.transform = "rotate(0deg)"
+    document.getElementById("whiteB").style.transform = "rotate(0deg)"
+    document.getElementById("blackB").style.transform = "rotate(0deg)"
+    } else {
     document.getElementById("productImg").src = "../img/rick-muigo-SX9IouL_qOg-unsplash_red.jpg"
 
     document.getElementById("redB").style.boxShadow = "0 0 0 3px black"
@@ -111,7 +198,7 @@ function toggleRed(desktopsize) {
     document.getElementById("blueB").style.transform = "rotate(0deg)"
     document.getElementById("whiteB").style.transform = "rotate(0deg)"
     document.getElementById("blackB").style.transform = "rotate(0deg)"
-
+    }
 
 
 // funkar
